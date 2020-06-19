b3d6389a978796180ad9eb1d15b84e6b49968ff2
Nir <nir.mtk@gmail.com>
2020-06-19 11:28:33 +0300

    Titles and grids
    Titles and grids were added to the code

--- ControlTheoryProj.m
+++ ControlTheoryProj.m
@@ -4,14 +4,22 @@ close all
 clc
 
 %% Transfer Functions Define:
-G1 = tf([1 2] , [1 2 4]);
-G2 = tf([1 10] , [1 5 4]);
-G3 = tf(1 , [1 4 5 0]);
+G1 = tf([1 2] , [1 2 4]); % (s+2)/(s^2+2s+4)
+G2 = tf([1 10] , [1 5 4]); % (s+10)/(s+1)(s+4)
+G3 = tf(1 , [1 4 5 0]); % 1/[s(s^2+4s+5)]
 
 %% Root Locus:
-figure()
+figure('Name','Q1')
 rlocus(G1);
-figure()
+grid on
+title('$\frac{s+2}{s^2+2s+4}$','Interpreter','latex')
+
+figure('Name','Q2')
 rlocus(G2);
-figure()
+grid on
+title('$\frac{s+10}{(s+1)(s+4)}$','Interpreter','latex')
+
+figure('Name','Q3')
 rlocus(G3);
+grid on
+title('$\frac{1}{s(s^2+4s+5)}$','Interpreter','latex')
